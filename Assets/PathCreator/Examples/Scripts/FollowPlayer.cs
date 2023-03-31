0a5a3ebfeafd968c038143a1be4fb4d4568f782a
Caitlyn-Milne <CaitlynMilne2001@gmail.com>
2023-04-01 00:53:03 +0100

    Changes for art style

---
+++ Assets/PathCreator/Examples/Scripts/FollowPlayer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using static UnityEditor.FilePathAttribute;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform Target;
+    public float Dampening;
+
+    private Vector3 velocity;
+    private Vector3 offset;
+    private Queue<Vector3> locations = new Queue<Vector3>();
+    public bool LockX;
+    public bool LockY;
+    public bool LockZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - Target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = LockX ? transform.position.x : Target.position.x + offset.x;
+        var y = LockY ? transform.position.y : Target.position.y + offset.y;
+        var z = LockZ ? transform.position.z : Target.position.z + offset.z;
+        var targetLocation = new Vector3(x, y, z);
+        if (Dampening == 0)
+        {
+            transform.position = targetLocation;
+            return;
+        }
+        transform.position = Vector3.SmoothDamp(transform.position, targetLocation, ref velocity, Dampening);
+    }
+}
